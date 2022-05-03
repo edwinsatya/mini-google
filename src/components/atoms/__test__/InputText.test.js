fbe9152920651df0e80e098896cb6f964a1d2b09
edwinsatya <edwinsatyayudistira@gmail.com>
2022-05-03 20:58:49 +0700

    update input text testing for check class disabled

--- src/components/atoms/__test__/InputText.test.js
+++ src/components/atoms/__test__/InputText.test.js
@@ -9,9 +9,13 @@ describe("InputText Atom Component", () => {
   });
 
   it("Should be render in disable condition", () => {
+    const classDisabed =
+      "w-full bg-gray-200 dark:bg-gray-800 hover:dark:bg-gray-700";
+
     render(<InputText disabled={true} />);
     const inputText = screen.getByTestId("input-text");
     expect(inputText).toBeDisabled();
+    expect(inputText).toHaveClass(classDisabed);
   });
 
   it("Should be render in enable condition", () => {
