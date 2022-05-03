fe2d72bcda738da2e73fe004db1cd626b55e9a74
edwinsatya <edwinsatyayudistira@gmail.com>
2022-05-03 20:37:39 +0700

    update input text testing without crashing

--- src/components/atoms/__test__/InputText.test.js
+++ src/components/atoms/__test__/InputText.test.js
@@ -1,9 +1,10 @@
-import { render } from "@testing-library/react";
+import { render, screen } from "@testing-library/react";
 import InputText from "../InputText.jsx";
 
 describe("Render input text atom component", () => {
   it("Should be render without crashing", () => {
-    const inputText = document.createElement("input");
-    render(<InputText />, inputText);
+    render(<InputText />);
+    const inputText = screen.getByTestId("input-text");
+    expect(inputText).toBeTruthy();
   });
 });
