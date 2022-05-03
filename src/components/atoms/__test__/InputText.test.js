fb56c01971fbba768507700670b6c5fb28cc7e76
edwinsatya <edwinsatyayudistira@gmail.com>
2022-05-03 21:32:39 +0700

    update input text testing for check event change correctly

--- src/components/atoms/__test__/InputText.test.js
+++ src/components/atoms/__test__/InputText.test.js
@@ -1,4 +1,4 @@
-import { render, screen } from "@testing-library/react";
+import { render, screen, fireEvent } from "@testing-library/react";
 import InputText from "../InputText.jsx";
 
 describe("InputText Atom Component", () => {
@@ -28,5 +28,18 @@ describe("InputText Atom Component", () => {
     render(<InputText type="text" />);
     const inputText = screen.getByTestId("input-text");
     expect(inputText).toHaveAttribute("type", "text");
+  });
+
+  it("Should be render with input event change correctly", () => {
+    render(<InputText type="text" onChange={(e) => e} />);
+    const inputText = screen.getByTestId("input-text");
+    const testValue = "edwin satya";
+    fireEvent.change(inputText, {
+      target: {
+        value: testValue,
+      },
+    });
+
+    expect(inputText).toHaveValue(testValue);
   });
 });
